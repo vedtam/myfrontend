857993a45e89919b02d07ab34aefa8a04c6b2a44
Edmond Varga <vedtam@yahoo.com>
2018-04-06 10:17:30 +0300

    fix

--- src/MyFrontendServiceProvider.php
+++ src/MyFrontendServiceProvider.php
@@ -8,8 +8,11 @@ class MyFrontendServiceProvider extends ServiceProvider
     public function boot()
     {
         PresetCommand::macro('myfrontend', function ($command) {
+          
             MyFrontendPreset::install();
+
             $command->info('My frontend scaffolding installed successfully.');
+            $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
     }
 }
